ce37b6fdcd3ddec1bcc5ccd42fddecaf92234f03
prChoe <massivcode@gmail.com>
2017-05-27 12:08:04 +0900

    function : MemoAdapter 연결 코드 작성 및 폼 입력 검증 코드 추가

--- app/src/main/java/com/study/branchexam2/MainActivity.java
+++ app/src/main/java/com/study/branchexam2/MainActivity.java
@@ -4,14 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText mTitleEditText;
     private EditText mContentsEditText;
     private RecyclerView mMemoRecyclerView;
+    private MemoAdapter mMemoAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +27,30 @@ public class MainActivity extends AppCompatActivity {
         mTitleEditText = (EditText) findViewById(R.id.main_title_et);
         mContentsEditText = (EditText) findViewById(R.id.main_contents_et);
         findViewById(R.id.main_memo_add_btn).setOnClickListener(mOnMemoAddClicked);
+        mMemoAdapter = new MemoAdapter();
         mMemoRecyclerView = (RecyclerView) findViewById(R.id.main_memo_rv);
         mMemoRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        mMemoRecyclerView.setAdapter(mMemoAdapter);
     }
 
     private View.OnClickListener mOnMemoAddClicked = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
+            String title = mTitleEditText.getText().toString();
 
+            if (TextUtils.isEmpty(title) || title.length() < 1) {
+                Toast.makeText(MainActivity.this, "제목을 입력하세요!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            String contents = mContentsEditText.getText().toString();
+
+            if (TextUtils.isEmpty(contents) || contents.length() < 1) {
+                Toast.makeText(MainActivity.this, "내용을 입력하세요!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Memo memo = new Memo(title, contents);
         }
     };
 }
